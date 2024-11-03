9609db6758d747d0c21ae51f652eb66195027c7e
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2024-11-03 10:11:46 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+function isValidIP(ip) {
+    const ipv4Pattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    const ipv6Pattern = /([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|::|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(1[0-9]{2}|[1-9]?[0-9]){1,2})\.){3}(25[0-5]|(1[0-9]{2}|[1-9]?[0-9]){1,2})|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(1[0-9]{2}|[1-9]?[0-9]){1,2})\.){3}(25[0-5]|(1[0-9]{2}|[1-9]?[0-9]){1,2})$/;
+    
+    return ipv4Pattern.test(ip) || ipv6Pattern.test(ip);
+}
+
 function getIPInfo() {
     var ipAddress = document.getElementById("ipInput").value;
+    
+    if (!isValidIP(ipAddress)) {
+        document.getElementById("ipInfo").innerHTML = "<p>Error: Invalid IP address format</p>";
+        return;
+    }
+
     fetch("https://ipapi.co/" + ipAddress + "/json/")
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) throw new Error("Network response was not ok");
+            return response.json();
+        })
         .then(data => {
+            if (data.error) {
+                throw new Error(data.error);
+            }
             var info = `
-                <p><b>Result:</b></p>
-                <br><br><br>
+                <p style="margin-bottom: 25px;"><b>Result:</b></p>
                 <p>IP: <span>${data.ip}</span></p>
                 <p>Country: <span>${data.country_name}</span></p>
                 <p>Region Code: <span>${data.region_code}</span></p>
@@ -18,26 +36,27 @@ function getIPInfo() {
             document.getElementById("ipInfo").innerHTML = info;
         })
         .catch(error => {
-            document.getElementById("ipInfo").innerHTML = "<p>Error: Invalid IP address</p>";
+            document.getElementById("ipInfo").innerHTML = `<p>Error: ${error.message}</p>`;
         });
 }
-document.getElementById("check-my-ip").addEventListener('click', function(){
-    async function getPublicIP() {
-    try {
-      const response = await fetch('https://api.ipify.org?format=json');
-      const data = await response.json();
 
-      return data.ip;
-    } catch (error) {
-      console.error('Error fetching public IP:', error);
-      return null;
+document.getElementById("check-my-ip").addEventListener("click", function () {
+    async function getPublicIP() {
+        try {
+            const response = await fetch("https://api.ipify.org?format=json");
+            const data = await response.json();
+            return data.ip;
+        } catch (error) {
+            console.error("Error fetching public IP:", error);
+            return null;
+        }
     }
-  }
+    
     getPublicIP().then(publicIP => {
-    if (publicIP) {
-      document.getElementById("ipInput").value = publicIP
-      console.log('Public IP:', publicIP);
-      getIPInfo();
-    }
-  });
+        if (publicIP) {
+            document.getElementById("ipInput").value = publicIP;
+            console.log("Public IP:", publicIP);
+            getIPInfo();
+        }
+    });
 });
